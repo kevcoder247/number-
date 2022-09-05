4883a0df0b8fbd75d57dcad6aa8a4c9fb58f68f5
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-09-05 15:16:29 -0400

    logic added: if player loses, background color changes to red

--- js/app.js
+++ js/app.js
@@ -41,18 +41,39 @@ guessBtn.addEventListener('click', () => {
 
     //Check if player won
     if(guess === winningNum){
-       //Disable input
-       guessInput.disabled = true;
-       //Change Background color when player wins
-       body.style.background = 'green';
-       setMessage(`You win the correct number is ${winningNum}`, 'black', '25px')
+        gameOver(true, )
     }else{
-
+        //Wrong Number
+        guessesLeft -= 1;
+        
+        if(guessesLeft === 0){
+            //Game over 
+            gameOver(false, `You lose, the correct answer is  ${winningNum}`, 'black', '25px')
+        }else{
+            //Game Continues- aswer wrong
+            setMessage(`${guess} is not correct, you have ${guessesLeft} guesses left`, 'black', '25px')
+            body.style.background = 'white';
+            //Clear input
+            guessInput.value = '';
+        }
     }
 });
 
-//Set Message
 
+//Game Over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
+    
+    //Disable input
+    guessInput.disabled = true;
+    //Change Background color when player wins
+    body.style.background = color;
+    setMessage(msg)
+}
+
+
+//Set Message
 function setMessage(msg, color, font){
     message.style.color = color;
     message.style.fontSize = font;

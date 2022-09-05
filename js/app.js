3141ada6e5ecc896192fc7207b77bb5b199c6dba
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-09-05 08:21:47 -0400

    Game logic added: if player guesses the right number background color changes and Player is notified

--- js/app.js
+++ js/app.js
@@ -15,12 +15,46 @@ let min = 1,
 
 
 //UI Elements
-const game = document.querySelector('#game');
+const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
       guessInput = document.querySelector('#guess-input'),
-      message = document.querySelector('.message');
+      message = document.querySelector('.message'),
+      body = document.querySelector('#body');
 
 
-      
+//Assign
+
+minNum.textContent  = min;
+maxNum.textContent = max;
+
+
+//Btn Event listener
+guessBtn.addEventListener('click', () => {
+    let guess = parseInt(guessInput.value);
+
+    //Validate
+    if( isNaN(guess) || guess < min || guess > max ){
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red', '20px')
+    }
+
+    //Check if player won
+    if(guess === winningNum){
+       //Disable input
+       guessInput.disabled = true;
+       //Change Background color when player wins
+       body.style.background = 'green';
+       setMessage(`You win the correct number is ${winningNum}`, 'black', '25px')
+    }else{
+
+    }
+});
+
+//Set Message
+
+function setMessage(msg, color, font){
+    message.style.color = color;
+    message.style.fontSize = font;
+    message.textContent = msg;
+}
